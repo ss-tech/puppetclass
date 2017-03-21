7cc2c5583e9b7316e9847c6018daaaf2b9c84052
coryschaubs <cory1@schaubs.com>
2017-03-21 14:31:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,14 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  notify{"Hello, my name is ${::hostname}":}
 }
+
+file {'/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => "Today I learned what it means to manage state using Puppet.\n",
+}
+  
