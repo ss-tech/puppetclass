e0e2c208605d1bc71f4eec2647f92b9b205674f1
allenmuller <allenmuller@hotmail.com>
2017-03-21 15:52:52 -0400

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,6 +1,7 @@
 class users {
 user { 'fundamentals':
 ensure => present,
+include users,
 # password => 'puppet8#labs', # Windows requires a plain text password
 # groups => ['Users'], # Display in Windows Control Panel
 }
