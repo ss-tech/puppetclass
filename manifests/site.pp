d940b9eac2b7b3751d546f2d23ec00eab786820e
morgantz <cgantz@hersheys.com>
2017-03-21 15:11:15 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,13 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  path => '/usr/local/bin',
+  creates => '/etc/motd',
   }
-  file { '/etc/motd':
-      ensure => file,
-      owner => 'root',
-      group => 'root',
-      mode => '0440',
-    content => "Think Keystone Positive Chris!",
 
+  
+ 
 }
 
