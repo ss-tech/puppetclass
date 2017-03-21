5930b90ddd1774349e5dcdd72f8396d8afc54e12
jkell009 <jdkelly@hersheys.com>
2017-03-21 14:49:54 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,11 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  file { '/etc/motd':  
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',  
+    content => "Hey, Puppet training is fun!\n",}
   include role::classroom
 }
