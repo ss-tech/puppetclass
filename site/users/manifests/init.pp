b7b68e0a8d6fbdbef690d2d9d86300e5fbeee508
allenmuller <allenmuller@hotmail.com>
2017-03-21 15:58:47 -0400

    8.2

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,8 +1,1 @@
-class users {
-user { 'fundamentals':
-ensure => present,
-include users,
-# password => 'puppet8#labs', # Windows requires a plain text password
-# groups => ['Users'], # Display in Windows Control Panel
-}
-}
+include users
