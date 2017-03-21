76d58ab7e02537c56135f3e4a38726ecf697bb1a
Bruce Brouwer <bruce.brouwer@gfs.com>
2017-03-21 15:44:55 -0400

    Remove wrong manifest

--- site/users/manifest/init.pp
+++
@@ -1,7 +0,0 @@
-class users {
-  user { 'fundamentals':
-    ensure => present,
-    # password => 'puppet8#labs', # Windows requires a plain text password
-    # groups => ['Users'], # Display in Windows Control Panel
-  }
-}
