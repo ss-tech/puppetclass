f6b1e3739a52d190cb689e80a8e4001dfe436e7e
pkoneti <padmashree.koneti@puppet.com>
2017-03-21 12:15:06 -0700

    lab 7.2 - idempotent execs

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  exec { cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-  path => '/usr/local/bin'
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  path => '/usr/local/bin',
   creates => '/etc/motd', 
   }
   include role::classroom
