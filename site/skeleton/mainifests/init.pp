9abb38e7f2d5c550a6cba61caeaa0f19e7b41ebd
psalomon2 <azand.aoc@comcast.net>
2017-03-22 09:42:28 -0400

    Update init.pp

--- site/skeleton/mainifests/init.pp
+++ site/skeleton/mainifests/init.pp
@@ -6,7 +6,7 @@ class skeleton {
     group => root,
     mode => 0755,
   }
-  file { '/etc/skel/.bashrc'
+  file { '/etc/skel/.bashrc':
      ensure => file,
      owner => root,
      group => root,
