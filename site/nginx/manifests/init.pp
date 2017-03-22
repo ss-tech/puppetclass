00af0c217a02ab3a0a32b2f3c572b2ff82b111f8
sswander1 <sswander.ss@gmail.com>
2017-03-22 16:48:03 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,33 +6,33 @@ case $facts['os']['family'] {
     $nginx_owner = 'root',
     $nginx_group = 'root',
     $nginx_www_dir = '/var/www'
-    $nginx_conf_root_dir = '/etc/nginx'
-    $nginx_conf_incl_dir = '/etc/nginx/conf.d'
+    $nginx_conf_root_dir = '/etc/nginx',
+    $nginx_conf_incl_dir = '/etc/nginx/conf.d',
     $nginx_log_dir = '/var/log/nginx',
     $nginx_service = 'nginx',
     $nginx_service_account = 'www-data',
   }
   'redhat': {
-    $nginx_package_name = 'nginx',
-    $nginx_owner = 'root',
-    $nginx_group = 'root',
+    $nginx_package_name = 'nginx'
+    $nginx_owner = 'root'
+    $nginx_group = 'root'
     $nginx_www_dir = '/var/www'
     $nginx_conf_root_dir = '/etc/nginx'
     $nginx_conf_incl_dir = '/etc/nginx/conf.d'
-    $nginx_log_dir = '/var/log/nginx',
-    $nginx_service = 'nginx',
-    $nginx_service_account = 'nginx',
+    $nginx_log_dir = '/var/log/nginx'
+    $nginx_service = 'nginx'
+    $nginx_service_account = 'nginx'
   }
   'windows': {
-    $nginx_package_name = 'nginx-service',
-    $nginx_owner = 'Administrator',
-    $nginx_group = 'Administrators',
+    $nginx_package_name = 'nginx-service'
+    $nginx_owner = 'Administrator'
+    $nginx_group = 'Administrators'
     $nginx_www_dir = 'C:/ProgramData/nginx/html'
     $nginx_conf_root_dir = 'C:/ProgramData/nginx'
     $nginx_conf_incl_dir = 'C:/ProgramData/nginx/conf.d'
-    $nginx_log_dir = 'C:/ProgramData/nginx/logs',
-    $nginx_service = 'nginx',
-    $nginx_service_account = 'nobody',
+    $nginx_log_dir = 'C:/ProgramData/nginx/logs'
+    $nginx_service = 'nginx'
+    $nginx_service_account = 'nobody'
   }
   default: {
     fail("Unsupported OS Family ${facts['os']['family']} ")
