d492c9fde27f5fd18d566589f339ed987d2d6e4b
sswander1 <sswander.ss@gmail.com>
2017-03-22 13:42:58 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,65 +1,39 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0664'
+  }
 
-  # Packages
+
   package { 'nginx':
-    ensure => latest,
+    ensure => present,
   }
-
-  # Directories
-  file { 'nginx.www.root':
+  file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755'
-    path => '/var/www',
   }
-  
-  file { 'nginx.config.root':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755'
-    path => '/etc/nginx',
-  }
-  
-  file { 'nginx.config.include' :
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755'
-    path => '/etc/nginx/conf.d,
-  }
-  
-  # Files
-  file { 'nginx.conf':
+  file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
-    require => File['nginx.config.root'],
     require => Package['nginx'],
-    notify => Service['nginx'],
+    notify  => Service['nginx'],
   }
-  
-  file { 'nginx.default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    path => '/etc/nginx/conf.d/default.conf',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => File['nginx.config.include'],
-    notify => Service['nginx'],
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    mode   => '0775',
   }
-
-
-
-
-
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
   service { 'nginx':
-    ensure  => running
-    enable    => true,
+    ensure => running,
+    enable => true,
   }
 }
