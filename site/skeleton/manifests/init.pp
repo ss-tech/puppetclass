7baa8b13b38b1c4d14668c5ddc17c82d53946bbf
sswander1 <sswander.ss@gmail.com>
2017-03-22 09:39:36 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,14 +2,12 @@ class skeleton{
   file { '/etc/skel':
     ensure => 'directory',
     group  => 'root',
-    mode   => '0755',
     owner  => 'root',
     type   => 'directory',
   }
   file { '/etc/skel/.bashrc':
     ensure  => 'file',
     group   => 'root',
-    mode    => '0644',
     owner   => 'root',
     source => 'puppet:///modules/skeleton/bashrc'
   }
