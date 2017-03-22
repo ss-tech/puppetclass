d4899589baca63be21f35cbd9c6e4d57b7725d46
Andrew Shewchenko <aluscia@gmail.com>
2017-03-22 14:13:38 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+if $::hostname =~ /.*?\.vm/ {
+  notify "This machine is a vm"
+}
+
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
