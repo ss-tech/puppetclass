609c845fb20d67ada487ccbe35e24202c07d57d7
pkoneti <padmashree.koneti@puppet.com>
2017-03-22 07:36:53 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include ::users
   include ::classroom
   include ::skeleton
 }
