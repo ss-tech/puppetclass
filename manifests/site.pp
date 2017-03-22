de1497554502e49d1573d19aeaee6ce7d3a4d220
coryschaubs <cory1@schaubs.com>
2017-03-22 14:12:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,6 +46,11 @@ node default {
   #include ::users
   include ::skeleton
   #notify{"Hello, my name is ${::hostname}":}
+
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
 }
 
 #file {'/etc/motd':
