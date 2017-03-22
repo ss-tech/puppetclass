8c19583480828cf35b1c32432f9b3fb96c29f0a1
Drew Nase <dnasecs@gmail.com>
2017-03-22 11:33:47 -0400

    lab 11.1 package | file | service

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,19 @@
+class memcached {
+  package { 'memcached':
+    ensure => latest,
+  }
+  file { 'memcached.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/sysconfig/memcached'
+    source => 'puppet:///modules/memcached/memcached.conf',
+    require => Package['memcached'],
+  }
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['memcached.conf'],
+  }
+}
