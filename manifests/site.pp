ec7bfde8abe51b67826d786de0792aea1316a577
sswander1 <sswander.ss@gmail.com>
2017-03-22 10:31:23 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  #include role::classroom
+  #include ::users
+  #include ::skeleton
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motdnew":
     path => '/usr/local/bin',
     creates => '/etc/motdnew',
