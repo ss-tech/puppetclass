73680cbc0836444579691fbc4436aadd5b9b99dd
hfeek001 <ekrout@hersheys.com>
2017-03-22 14:13:19 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,3 +49,12 @@ node default {
   include role::classroom
   include ::users
 }
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+}
