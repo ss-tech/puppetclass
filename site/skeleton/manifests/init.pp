a14c27db5775d438a6a2641322e87ded32f16e52
jnance86 <jacobh.nance@gmail.com>
2017-03-22 08:50:11 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,10 +1,8 @@
 class skeleton {
 file { '/etc/skel':
   ensure => 'directory',
-  ctime  => '2017-03-20 13:53:40 +0000',
   group  => '0',
   mode   => '0777',
-  mtime  => '2017-03-20 13:53:27 +0000',
   owner  => '0',
   }
 file { '/etc/skel/.bashrc': 
