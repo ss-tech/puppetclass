ff19a25a932b8ca312243ec6874666c90659cd1f
esanchezvela <esanchezvela@gmail.com>
2017-03-22 09:25:37 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,14 @@
 
+class skeleton {
+    file { "/etc/skel":
+        ensure => 'directory',
+    }
+    
+    file { "/etc/skel/.bashrc":
+        ensure => 'file',
+        source => "puppet:///skeleton/bashrc",
+        require => File['/etc/skel'],
+    }
+}
+    
+            
