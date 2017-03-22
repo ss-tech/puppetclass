071210366fa46e9ac385c54822cc39b7ce3d875c
Chad Williams <chad.williams@gfs.com>
2017-03-22 11:37:37 -0400

    new

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,9 +1,9 @@
 class memcached {
-package { 'memcached';
+package { 'memcached':
   ensure => present,
 }
 
-file { '/etc/sysconfig/memcached';
+file { '/etc/sysconfig/memcached':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
@@ -13,7 +13,7 @@ file { '/etc/sysconfig/memcached';
 }
 }
 
-service { 'memcached';
+service { 'memcached':
   ensure    => running,
   enable    => true,
   subscribe => File['/etc/sysconfig/memcached'],
