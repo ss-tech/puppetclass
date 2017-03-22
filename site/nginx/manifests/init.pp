bf92dde57f8af33a2ca564f5aa4662f3d789d395
sswander1 <sswander.ss@gmail.com>
2017-03-22 16:56:18 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -65,18 +65,18 @@ case $facts['os']['family'] {
   file { [ "${nginx_www_dir}", "${nginx_conf_root_dir}", "${nginx_conf_incl_dir}" ]:
     ensure => directory,
   }
-  file { "${nginx_www_dir}"/"${nginx_index_file}":
+  file { "${nginx_www_dir}/${nginx_index_file}":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { "${nginx_conf_root_dir}"/"${nginx_conf_file}":
+  file { "${nginx_conf_root_dir}/${nginx_conf_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  file { "${nginx_conf_incl_dir}"/"${nginx_default_file}":
+  file { "${nginx_conf_incl_dir}/${nginx_default_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
