8f727d1b72e311d45893513cc5bb20271714318d
sswander1 <sswander.ss@gmail.com>
2017-03-22 13:53:40 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,7 +3,9 @@ class nginx {
   $nginx_www_dir = '/var/www'
   $nginx_conf_root_dir = '/etc/nginx'
   $nginx_conf_incl_dir = '/etc/nginx/conf.d'
-
+  $nginx_index_file = 'index.html'
+  $nginx_conf_file = 'nginx.conf'
+  $nginx_default_file = 'default.conf'
 
   File {
     owner => 'root',
@@ -18,18 +20,18 @@ class nginx {
   file { [ $nginx_www_dir, $nginx_conf_root_dir, $nginx_conf_incl_dir ]:
     ensure => directory,
   }
-  file { '/var/www/index.html':
+  file { "${nginx_www_dir}/${nginx_index_file}":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { '/etc/nginx/nginx.conf':
+  file { ${nginx_conf_root_dir}/${nginx_conf_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
+  file { ${nginx_conf_incl_dir}/${nginx_default_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
