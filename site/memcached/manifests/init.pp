b76fc712d18d4016c9c4bcace605cae1ae187b08
psalomon2 <azand.aoc@comcast.net>
2017-03-22 11:36:52 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,20 @@
 
+class memcached {
+  package { 'memcached':
+    ensure => latest,
+  }
+  file { 'memcached.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/sysconfig/memcached',
+    source => 'puppet:///modules/memcached/memcached.conf',
+    requires => Package['memcached'],
+  }
+  service { 'memcache':
+    ensure => running,
+    enable => true,
+    subscribes => File['/etc/sysconfig/memcached'],
+  }
+}
