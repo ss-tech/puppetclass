eebee08648f61203fa063dc9aa55195fb0e48066
psalomon2 <azand.aoc@comcast.net>
2017-03-22 14:20:06 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   if $::virtual != 'physical' {
      $vmname = capitalize($::virtual)
-     notify {"This is a ${vmname} virtual machine"}
+     notify {"This is a ${vmname} virtual machine.": }
   }
   
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
