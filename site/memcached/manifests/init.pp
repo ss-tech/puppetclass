a95ee3c83eb99dccdce1232bec746bb14ad2e10e
allenmuller <allenmuller@hotmail.com>
2017-03-22 11:34:50 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -4,7 +4,7 @@ class memcached {
   }
 file { 'memcached.conf':
   ensure => file,
-  path => '/etc/sysconfig/memcached'
+  path => '/etc/sysconfig/memcached',
   owner => 'root',
   group => 'root',
   mode => '0644',
