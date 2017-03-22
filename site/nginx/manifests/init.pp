4f853b97a4ad56a320a6d405cc92e6fbd989ebde
sswander1 <sswander.ss@gmail.com>
2017-03-22 13:21:30 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,64 @@
+class nginx {
+
+  # Packages
+  package { 'nginx':
+    ensure => latest,
+  }
+
+  # Directories
+  file { 'nginx.www.root':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755'
+    path => '/var/www',
+  }
+  
+  file { 'nginx.config.root':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    path => '/etc/nginx',
+  }
+  
+  file { 'nginx.config.include':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    path => '/etc/nginx/conf.d',
+  }
+
+  # Files
+  file { 'nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/nginx/nginx.conf',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => File['nginx.config.root'],
+    require => Package['nginx'],
+  }
+  
+  file { 'nginx.default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/nginx/conf.d/default.conf',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => File['nginx.config.include'],
+  }
+
+
+
+
+
+  service { 'nginx':
+    ensure  => running
+
+  }
+
+}
