312c5b1806166e51177ce6fd04a763364a2c4895
sswander1 <sswander.ss@gmail.com>
2017-03-22 11:26:04 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,20 +1,22 @@
-# package
-package { 'memcached':
-  ensure => latest,
-}
+class memcached {
+  # package
+  package { 'memcached':
+    ensure => latest,
+  }
 
-# file
-file { '/etc/sysconfig/memcached':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  source  => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'],
-  notify  => Service['memcached'],
-}
+  # file
+  file { '/etc/sysconfig/memcached':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    notify  => Service['memcached'],
+  }
 
-# service
-service { 'memcached':
-  ensure  => running,
-  enable  => true,
+  # service
+  service { 'memcached':
+    ensure  => running,
+    enable  => true,
+  }
 }
