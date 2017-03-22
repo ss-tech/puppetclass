3e74f96a5282d271c44e16da18579b0ed3c45f6d
thedarkwriter <jvftuo@gmail.com>
2017-03-22 17:38:36 +0000

    lab 12.1 nginx

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,7 @@ node default {
   include ::role::classroom
   include ::skeleton
   include ::memcached
+  include ::nginx
 
  # file { "/etc/motd":
  #   ensure  => file,
---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,36 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
