5f38803bfd82f6b190578192ee82e62261297aa6
jkell009 <jdkelly@hersheys.com>
2017-03-22 11:44:21 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,7 +2,7 @@ class memcached {
  package { 'memcached':
   ensure => present,
  }
- file { 'memcached.conf':
+ file { '/etc/sysconfig/memcached':
   ensure => file,
   owner => 'root',
   group => 'root',
@@ -13,6 +13,6 @@ class memcached {
  service { 'memcached':
   ensure => running,
   enable => true,
-  subscribe => File['memcached.conf'],
+  subscribe => File['/etc/sysconfig/memcached'],
  }
 }
