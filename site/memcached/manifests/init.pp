6a2d7c9fc0bec54729a36fa8dabbe2fa1df3a49a
jnance86 <jacobh.nance@gmail.com>
2017-03-22 10:30:51 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,7 +2,7 @@ class memcached {
 package { 'memcached': 
   ensure => present,
   }
-file {'/etc/systemconfig/memcached':
+file { '/etc/systemconfig/memcached':
   ensure => file,
   owner => 'root',
   group => 'root',
