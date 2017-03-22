32f6bef7643b56dbbddecf3f4461e0e2ee32af68
thedarkwriter <jvftuo@gmail.com>
2017-03-22 13:49:20 +0000

    Adding user frank

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,5 +2,11 @@ class users {
   user{ "fundamentals":
     ensure => 'present',
   }
+  user{ "frankthetank":
+    ensure     => 'present',
+    name       => 'frank',
+    password   => '!Q2w3e4r',
+    managehome => true,
+    
 }
 
