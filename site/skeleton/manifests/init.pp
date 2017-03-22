5ac14da6d60469bb921c05b6a677d4f612ebd7fb
psalomon2 <azand.aoc@comcast.net>
2017-03-22 09:56:22 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,14 +2,14 @@
 class skeleton {
   file { '/etc/skel':
     ensure => directory,
-    owner => root,
-    group => root,
+    owner => 'root',
+    group => 'root',
     mode => 0755,
   }
   file { '/etc/skel/.bashrc':
      ensure => file,
-     owner => root,
-     group => root,
+     owner => 'root',
+     group => 'root',
      mode => 0644,
      source => 'puppet:///modules/skeleton/bashrc',
   }
