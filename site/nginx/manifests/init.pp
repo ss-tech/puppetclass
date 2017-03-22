5ff92e4b459409b08be33f6e7a1e2cb477df36c5
sswander1 <sswander.ss@gmail.com>
2017-03-22 16:53:18 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -59,24 +59,24 @@ case $facts['os']['family'] {
   }
 
 
-  package { ${nginx_package_name}:
+  package { "{$nginx_package_name}":
     ensure => present,
   }
-  file { [ $nginx_www_dir, $nginx_conf_root_dir, $nginx_conf_incl_dir ]:
+  file { [ "${nginx_www_dir}", "${nginx_conf_root_dir}", "${nginx_conf_incl_dir}" ]:
     ensure => directory,
   }
-  file { "${nginx_www_dir}/${nginx_index_file}":
+  file { "${nginx_www_dir}"/"${nginx_index_file}":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { "${nginx_conf_root_dir}/${nginx_conf_file}":
+  file { "${nginx_conf_root_dir}"/"${nginx_conf_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  file { "${nginx_conf_incl_dir}/${nginx_default_file}":
+  file { "${nginx_conf_incl_dir}"/"${nginx_default_file}":
     ensure  => file,
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
