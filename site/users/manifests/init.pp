5f4f6890a801985e401584447612e8c8eb550c90
thedarkwriter <jvftuo@gmail.com>
2017-03-22 14:47:05 +0000

    Added darkwriter user

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -8,5 +8,9 @@ class users {
     password   => '!Q2w3e4r',
     managehome => true,
   }  
+  user { "thedarkwriter":
+    ensure     => 'present',
+    managehome => true,
+    password   => '!Q2w3e4r',
 }
 
