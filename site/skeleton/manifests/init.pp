69e13b5d54654a828f5aa93e8a7d33fa57bf6bbb
jnance86 <jacobh.nance@gmail.com>
2017-03-22 08:42:37 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -3,7 +3,7 @@ file { '/etc/skel':
   ensure => 'directory',
   ctime  => '2017-03-20 13:53:40 +0000',
   group  => '0',
-  mode   => '0755',
+  mode   => '0777',
   mtime  => '2017-03-20 13:53:27 +0000',
   owner  => '0',
   type   => 'directory',
