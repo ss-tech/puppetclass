84a8214e67c7b084ea46f6435e671d4709958735
Chad Williams <chad.williams@gfs.com>
2017-03-22 11:39:36 -0400

    fix

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,21 +1,20 @@
 class memcached {
-package { 'memcached':
-  ensure => present,
-}
+  package { 'memcached':
+    ensure => present,
+  }
 
-file { '/etc/sysconfig/memcached':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'],
-}
-}
+  file { '/etc/sysconfig/memcached':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+  }
 
-service { 'memcached':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/sysconfig/memcached'],
-}
+  service { 'memcached':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
 }
