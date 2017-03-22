539490c64668c29c1b4765857f1ee0dacad588e6
Bruce Brouwer <bruce.brouwer@gmail.com>
2017-03-22 10:26:10 -0400

    lab 10.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
-  include ::users
-  include ::skeleton
+  # include role::classroom
+  # include ::users
+  # include ::skeleton
 }
