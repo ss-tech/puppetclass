4c11316f69b7675c297a815b6e36bb6a232f7b68
sswander1 <sswander.ss@gmail.com>
2017-03-22 16:39:30 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,15 +1,53 @@
 class nginx {
 
-  $nginx_service_account = $facts['os']['name'] ? {
-    'ubuntu' => 'nginx',
-    'redhat' => 'nginx',
-    'windows' => 'noboxy',
-    default  => 'nginx',
+case $facts['os']['family'] {
+  'debian': {
+    $nginx_package_name = 'nginx',
+    $nginx_owner = 'root',
+    $nginx_group = 'root',
+    $nginx_www_dir = '/var/www'
+    $nginx_conf_root_dir = '/etc/nginx'
+    $nginx_conf_incl_dir = '/etc/nginx/conf.d'
+    $nginx_log_dir = '/var/log/nginx',
+    $nginx_service = 'nginx',
+    $nginx_service_account = 'www-data',
+  }
+  'redhat': {
+    $nginx_package_name = 'nginx',
+    $nginx_owner = 'root',
+    $nginx_group = 'root',
+    $nginx_www_dir = '/var/www'
+    $nginx_conf_root_dir = '/etc/nginx'
+    $nginx_conf_incl_dir = '/etc/nginx/conf.d'
+    $nginx_log_dir = '/var/log/nginx',
+    $nginx_service = 'nginx',
+    $nginx_service_account = 'nginx',
+  }
+  'windows': {
+    $nginx_package_name = 'nginx-service',
+    $nginx_owner = 'Administrator',
+    $nginx_group = 'Administrators',
+    $nginx_www_dir = 'C:/ProgramData/nginx/html'
+    $nginx_conf_root_dir = 'C:/ProgramData/nginx'
+    $nginx_conf_incl_dir = 'C:/ProgramData/nginx/conf.d'
+    $nginx_log_dir = 'C:/ProgramData/nginx/logs',
+    $nginx_service = 'nginx',
+    $nginx_service_account = 'nobody',
+  }
+  default: {
+    fail("Unsupported OS Family ${facts['os']['family']} ")
   }
 
-  $nginx_www_dir = '/var/www'
-  $nginx_conf_root_dir = '/etc/nginx'
-  $nginx_conf_incl_dir = '/etc/nginx/conf.d'
+
+#  
+#  $nginx_service_account = $facts['os']['name'] ? {
+#    'ubuntu' => 'nginx',
+#    'redhat' => 'nginx',
+#    'windows' => 'noboxy',
+#    default  => 'nginx',
+#  }
+
+
   $nginx_index_file = 'index.html'
   $nginx_conf_file = 'nginx.conf'
   $nginx_default_file = 'default.conf'
