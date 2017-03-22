81be12bdaf82055c0a4233c4a04ca24ca718c51c
esanchezvela <esanchezvela@gmail.com>
2017-03-22 09:34:27 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,11 +2,17 @@
 class skeleton {
     file { "/etc/skel":
         ensure => directory,
+        owner => 'root',
+        group => 'root',
+        mode  => '0755',
     }
     
     file { "/etc/skel/.bashrc":
         ensure => file,
         source => 'puppet:///modules/skeleton/bashrc',
+        owner => 'root',
+        group => 'root',
+        mode  => '0755',
         require => File['/etc/skel'],
     }
 }
