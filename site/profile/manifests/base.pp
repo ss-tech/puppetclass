a2344f1c9bb896c7f3a3fe45d04d3bb701f2f095
Chad Williams <chad.williams@gfs.com>
2017-03-23 10:31:44 -0400

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,6 @@
 class profile::base {
   notify { "Hello, my name is ${::hostname}": }
-  # /etc/puppetlabs/code/environments/production/hieradata/common.yaml
-  message: "This is a sample variable that came from Hiera"
+  
+  $message = hiera('message')
+  notify { $message: }
 }
