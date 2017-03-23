25d34ed576dfd53c6a8aa7ec1d6e3715a09456f4
Drew Nase <dnasecs@gmail.com>
2017-03-23 11:28:29 -0400

    lab 17.1 - parameterized classes

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,10 +1,12 @@
-class nginx {
+class nginx (
+  $root = undef,
+) {
   case $facts['os']['family'] {
     'redhat','debian' : {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      $default_docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
@@ -12,7 +14,7 @@ class nginx {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      $default_docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
@@ -26,6 +28,12 @@ class nginx {
     'debian' => 'www-data',
     'windows' => 'nobody',
   }
+  
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
+  }
+  
   File {
     owner => $owner,
     group => $group,
