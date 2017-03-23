b873a0c0022a6e80f44baf1ad3108074c514e57c
hfeek001 <ekrout@hersheys.com>
2017-03-23 11:51:47 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,35 +1,82 @@
-class nginx {
+class nginx (
+$root = undef,
+) {
+case $facts['os']['family'] {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+# $docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
+}
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+# $docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
+}
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+Lab 17.1: Proposed Solution
+VirtualFundamentals v1.1.4 28 ©2017 Puppet, Inc.
+# Set the default port for default.conf.epp template
+$port = '80'
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+# if $root isn't set, then fall back to the platform default
+$docroot = $root ? {
+undef => $default_docroot,
+default => $root,
+}
 File {
-owner => 'root',
-group => 'root',
+owner => $owner,
+group => $group,
 mode => '0664',
 }
-package { 'nginx':
+package { $package:
 ensure => present,
 }
-file { [ '/var/www', '/etc/nginx/conf.d' ]:
+file { [ $docroot, "${confdir}/conf.d" ]:
 ensure => directory,
 }
-file { '/var/www/index.html':
+file { "${docroot}/index.html":
 ensure => file,
 source => 'puppet:///modules/nginx/index.html',
 }
-file { '/etc/nginx/nginx.conf':
+file { "${confdir}/nginx.conf":
 ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
 notify => Service['nginx'],
 }
-file { '/etc/nginx/conf.d/default.conf':
+file { "${confdir}/conf.d/default.conf":
 ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
+content => epp('nginx/default.conf.epp',
+{
+port => $port,
+docroot => $docroot,
+}),
 notify => Service['nginx'],
-require => Package['nginx'],
 }
 service { 'nginx':
 ensure => running,
 enable => true,
 }
 }
-Lab 12.1: Proposed Solution
-VirtualFundamental
