e5d227f454ca74598459b03171712bb0cc8d8e26
Andrew Shewchenko <aluscia@gmail.com>
2017-03-23 09:36:35 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -3,4 +3,16 @@ define user::managed_user (
   $home = "/home/${title}",
   $group = $title,
   $ssh = "/home/${title}/.ssh",
-)
+) {
+  user { $title:
+    ensure => present,
+    home => $home,
+    managehome => true,
+  }
+  file { $ssh:
+    ensure => directory,
+    mode => '0700',
+    owner => $user,
+    group => $group,
+  }
+}
