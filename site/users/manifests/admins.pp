1b59b92bc9110875f2fac2c4eec0215e5abf0459
sswander1 <sswander.ss@gmail.com>
2017-03-23 10:05:39 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -2,12 +2,15 @@ class users::admins {
   users::managed_user { 'someadmin1':
     group => 'admins',
   }
+  
   users::managed_user { 'someadmin2':
     group => 'admins',
   }
-  users::managed_user { 'someadmin3:
+  
+  users::managed_user { 'someadmin3':
     group => 'ltdadmin',
   }
+  
   group { 'admins':
     ensure  => present,
   }
