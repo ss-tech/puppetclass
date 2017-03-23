e3bd2f1879c31c7c89a813bcb7afb7d3731a7b21
Bruce Brouwer <bruce.brouwer@gmail.com>
2017-03-23 15:33:56 -0400

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,3 +1,19 @@
 class profile::wordpress {
+  class { 'myslq::server':
+    root_password => 'supersekrit',
+  }
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
+  
+  include apache
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port => '80',
+    priority => '00',
+    docroot => '/opt/wordpress',
+    }
+  }
+
   class { 'wordpress': }
 }
