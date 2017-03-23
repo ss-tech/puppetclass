dc857dfe07eb4b87671385b18ee913a85f9083f2
jnance86 <jacobh.nance@gmail.com>
2017-03-23 12:06:36 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,15 @@
 class nginx (
-  $root = undef,
+ $package = $nginx::params::package, 
+ $owner = $nginx::params::owner,
+ $group = $nginx::params::group,
+ $docroot = $nginx::params::docroot,
+ $confdir = $nginx::params::confdir,
+ $logdir = $nginx::params::logdir,
+ $user = $nginx::params::user,
+ $port = $nginx::params::port,
 ){
-case $facts['os']['name'] {
-  'redhat','debian' :{
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-#      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-      $default_docroot ='/var/www'
-      }
-  'windows' :{
-    $package = 'nginx-service'
-    $owner = 'Administrator'
-    $group = 'Administrators'
-#    $docroot = 'C:/ProgramData/nginx/html'
-    $confdir = 'C:/ProgramData/nginx'
-    $logdir = 'C:/ProgramData/nginx/logs'
-    $default_docroot = 'C:/ProgramData/nginx/html'
-    }
-  default :{
-    fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-    }
+
 }
-$user = $facts['os']['family'] ? {
-  'redhat' => 'nginx',
-  'debian' => 'www-data',
-  'windows' => 'nobody',
-  }
 $port = '80'
 $docroot = $root ? {
 undef => $default_docroot,
