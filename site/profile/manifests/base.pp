608890fe4d5e15eb6e27c05cf011feb87dcc4d71
herpmm80 <paulmiller@hersheys.com>
2017-03-23 10:33:51 -0400

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,5 @@
 class profile::base {
-  notify { "Hello, my name is ${::hostname}": }
-  $message = hiera('message')
+  #notify { "Hello, my name is ${::hostname}": }
+  $message = hiera('message'),
   notify {$message}
 }
