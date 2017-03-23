61a6c07f1010ebf89689750070b0ea00f7b3b84d
philmabee <philmabee@gmail.com>
2017-03-23 11:26:24 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   include skeleton
+  class { 'nginx':
+    root => '/var/www/html',
+  }
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
     notify { "This is a ${vmname} virtual machine.": }
