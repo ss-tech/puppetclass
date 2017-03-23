078efc96fa2fc60864d7b6b52ad8a178c556a333
jkell009 <jdkelly@hersheys.com>
2017-03-23 12:10:03 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,9 +48,7 @@ node default {
  #   group   => 'root',
  #   mode    => '0644',  
  #   content => "Hey, Puppet training is fun!\n",}
-  class {'nginx':
-    root => '/var/www/html',
-   }
+  include ::nginx
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path => '/usr/local/bin',
     creates => '/etc/motd',
