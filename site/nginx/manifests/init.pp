a39032d13445391923c75afe6a9fa8165766959d
Drew Nase <dnasecs@gmail.com>
2017-03-23 08:58:00 -0400

    homework 2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,31 @@
 class nginx {
+  case $facts['os']['family'] {
+    'redhat','debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+    }
+  }
+
+  $user = $facts['os']['family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
   File {
     owner => 'root',
     group => 'root',
@@ -14,14 +41,22 @@ class nginx {
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { '/etc/nginx/nginx.conf':
+  file { "${confdir}/nginx.conf":
     ensure  => file,
-    source  => 'puppet:///modules/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp',
+    {
+      user => $user,
+      confdir => $confdir,
+      logdir => $logdir,
+    }),
     require => Package['nginx'],
   }
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${confdir}/conf.d/default.conf":
     ensure  => file,
-    source  => 'puppet:///modules/nginx/default.conf',
+    content => epp('nginx/default.conf.epp',
+    {
+      docroot => $docroot,
+    }),
     require => Package['nginx'],
   }
   service { 'nginx':
