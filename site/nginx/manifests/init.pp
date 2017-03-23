64cff160b03a9f7ca243c7d57edf63548cdd8214
herpmm80 <paulmiller@hersheys.com>
2017-03-23 11:58:13 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,13 @@
-class nginx {
-  case $facts['os']['family'] {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-      }
-
-    'windows' : {
-      $package = 'nginx-service'
-    $owner = 'Administrator'
-    $group = 'Administrators'
-    $docroot = 'C:/ProgramData/nginx/html'
-    $confdir = 'C:/ProgramData/nginx'
-    $logdir = 'C:/ProgramData/nginx/logs'
-    }
-
-  default : {
-fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-}
-}
-# user the service will run as. Used in the nginx.conf.epp template
-$user = $facts['os']['family'] ? {
-'redhat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-}
+ class nginx (
+ $package = nginx::params::package,
+ $owner = nginx::params::owner,
+ $group = nginx::params::root,
+ $docroot = nginx::params::docroot,
+ $confdir = nginx::params::confdir,
+ $logdir = nginx::params::logdir,
+ $user = nginx::params::user,
+ )
+ inherits nginx::params{
 File {
 owner => $owner,
 group => $group,
