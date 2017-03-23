ba6e3d0c96f94668dc7eda65627720145e3969b3
jnance86 <jacobh.nance@gmail.com>
2017-03-23 08:37:47 -0500

    Create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,9 @@
+class users::admins {
+  users:managed_user { 'joe':}
+  users:managed_user { 'alice', 'aaron':
+    group => 'staff',
+    }
+  group { 'staff':
+    ensure => 'present',
+    }
+    }
