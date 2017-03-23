542a5550171ce98f330ac828b9c87b047828e02c
esanchezvela <esanchezvela@gmail.com>
2017-03-23 10:51:38 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -2,14 +2,16 @@ class users::admins {
   users::managed_user { 'jose': 
       require => Group["jose"],
   }
-  #users::managed_user { ['alice','chen']: 
-  #    usergroup => 'staff',
-  #    require => Group["staff"],
-  #}
   
-  #group { 'staff':
-  #    ensure => present,
-  #}
+  users::managed_user { ['alice','chen']: 
+      usergroup => 'staff',
+      require => Group["staff"],
+  }
+  
+  group { 'staff':
+      ensure => present,
+  }
+  
   group { "jose":
       ensure => present,
   }
