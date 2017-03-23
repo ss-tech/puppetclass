4653f014b07d63e944b2f1a36bae29b2c5bb9909
allenmuller <allenmuller@hotmail.com>
2017-03-23 11:21:57 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,28 @@
-class nginx {
-File {
-owner => 'root',
-group => 'root',
-mode => '0664',
+class nginx (
+  $root = undef,
+) {
+ case $facts['os']['family'] {
+   'redhat','debian' : {
+   $package = 'nginx'
+   $owner = 'root'
+   $group = 'root'
+# $docroot = '/var/www'
+   $confdir = '/etc/nginx'
+   $logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+   $default_docroot = '/var/www'
 }
-package { 'nginx':
-ensure => present,
+ 'windows' : {
+   $package = 'nginx-service'
+   $owner = 'Administrator'
+   $group = 'Administrators'
+# $docroot = 'C:/ProgramData/nginx/html'
+   $confdir = 'C:/ProgramData/nginx'
+   $logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+   $default_docroot = 'C:/ProgramData/nginx/html'
 }
-file { [ '/var/www', '/etc/nginx/conf.d' ]:
-ensure => directory,
+ default : {
+ fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+ }
 }
-file { '/var/www/index.html':
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { '/etc/nginx/nginx.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
-}
-file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-notify => Service['nginx'],
-require => Package['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
-}
-
