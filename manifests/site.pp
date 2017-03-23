db5f36f0c1260b47bf006eaa8c55e902affd6d3e
Bruce Brouwer <bruce.brouwer@gmail.com>
2017-03-23 11:27:04 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,6 +37,11 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
+class profile::nginx {
+  class { 'nginx':
+    root => '/var/www',
+  }
+}
 
 node default {
   # This is where you can declare classes for all nodes.
@@ -47,9 +52,7 @@ node default {
   include ::skeleton
   include ::memcached
   include ::users::admins
-  class { 'nginx':
-    root => '/var/www',
-  }
+  include profile::nginx
   
   if $::is_virtual {
     $vmname = capitalize($::virtual)
