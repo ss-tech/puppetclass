9fb7fad01b5321eece466a2f01f65e3fab44c506
esanchezvela <esanchezvela@gmail.com>
2017-03-23 13:30:08 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -4,6 +4,7 @@ class nginx::params {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
+      $docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
