9fb4d33871796ca04a7da87d60a83fff17be31ea
sswander1 <sswander.ss@gmail.com>
2017-03-23 11:29:14 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,11 +1,13 @@
-class nginx {
+class nginx (
+  $root = undef,
+) {
 
 case $facts['os']['family'] {
   'debian': {
     $nginx_package_name = 'nginx'
     $nginx_owner = 'root'
     $nginx_group = 'root'
-    $nginx_www_dir = '/var/www'
+    $default_docroot = '/var/www'
     $nginx_conf_root_dir = '/etc/nginx'
     $nginx_conf_incl_dir = '/etc/nginx/conf.d'
     $nginx_log_dir = '/var/log/nginx'
@@ -16,7 +18,7 @@ case $facts['os']['family'] {
     $nginx_package_name = 'nginx'
     $nginx_owner = 'root'
     $nginx_group = 'root'
-    $nginx_www_dir = '/var/www/'
+    $default_docroot = '/var/www/'
     $nginx_conf_root_dir = '/etc/nginx'
     $nginx_conf_incl_dir = '/etc/nginx/conf.d'
     $nginx_log_dir = '/var/log/nginx/'
@@ -27,7 +29,7 @@ case $facts['os']['family'] {
     $nginx_package_name = 'nginx-service'
     $nginx_owner = 'Administrator'
     $nginx_group = 'Administrators'
-    $nginx_www_dir = 'C:/ProgramData/nginx/html'
+    $default_docroot = 'C:/ProgramData/nginx/html'
     $nginx_conf_root_dir = 'C:/ProgramData/nginx'
     $nginx_conf_incl_dir = 'C:/ProgramData/nginx/conf.d'
     $nginx_log_dir = 'C:/ProgramData/nginx/logs'
