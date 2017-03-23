3e54a9978325e4fc001194f8d816729e25911dbf
esanchezvela <esanchezvela@gmail.com>
2017-03-23 09:54:48 -0400

    ex15.1

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,27 @@
+define users::managed_user (
+    $homedir = "/home/"${title},
+    $usergroup = $title,
+) {
+    user { $title:
+        ensure      => present,
+        home        => $homedir,
+        manage_home => true,
+        gid         =>  $usergroup,
+    }
+    
+    file { "${homedir}/.ssh":
+        ensure  => directory,
+        owner   => $title,
+        group   => $usergroup,
+        mode    => '0700',
+    }
+    
+    file { "${homedir}/.bashrc":
+        ensure    => file,
+        owner   =>  $title,
+        group   =>  $usergroup,
+        mode    =>  "0700",
+        content =>  "echo 'Have a nice day' | cowsay",
+    }
+
+}
